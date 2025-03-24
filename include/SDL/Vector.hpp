300bcecf952a771526c1d19becc98fcafdbc97f1
PizzacKat <ion699503@gmail.com>
2025-03-24 18:57:50 +0100

    Added Cross and Dot products + CamelCase names

--- include/SDL/Vector.hpp
+++ include/SDL/Vector.hpp
@@ -1,5 +1,6 @@
 ﻿#ifndef SYSTEM_HPP
 #define SYSTEM_HPP
+#include <cmath>
 #include <type_traits>
 #include <SDL3/SDL_rect.h>
 
@@ -8,108 +9,116 @@ namespace SDL {
     struct Vector2 {
         static_assert(std::is_integral_v<T> || std::is_floating_point_v<T>, "T must be a number type");
 
-        Vector2(): x(0), y(0) {
+        constexpr Vector2(): x(0), y(0) {
 
         }
 
-        Vector2(T x, T y): x(x), y(y) {
+        constexpr Vector2(T x, T y): x(x), y(y) {
 
         }
 
         template <typename N>
-        Vector2(const Vector2<N> &other): x(static_cast<T>(other.x)), y(static_cast<T>(other.y)) {
+        constexpr Vector2(const Vector2<N> &other): x(static_cast<T>(other.x)), y(static_cast<T>(other.y)) {
 
         }
 
-        Vector2(SDL_Point point): x(static_cast<T>(point.x)), y(static_cast<T>(point.y)) {
+        constexpr Vector2(SDL_Point point): x(static_cast<T>(point.x)), y(static_cast<T>(point.y)) {
 
         }
 
-        Vector2(SDL_FPoint point): x(static_cast<T>(point.x)), y(static_cast<T>(point.y)) {
+        constexpr Vector2(SDL_FPoint point): x(static_cast<T>(point.x)), y(static_cast<T>(point.y)) {
 
         }
 
-        operator SDL_Point() const {
+        constexpr operator SDL_Point() const {
             return SDL_Point(static_cast<int>(x), static_cast<int>(y));
         }
 
-        operator SDL_FPoint() const {
+        constexpr operator SDL_FPoint() const {
             return SDL_FPoint(static_cast<float>(x), static_cast<float>(y));
         }
 
-        Vector2 operator+(const Vector2 &other) const {
-            return Vector2(this->x + other.x, this->y + other.y);
+        constexpr Vector2 operator+(const Vector2 &other) const {
+            return Vector2(x + other.x, y + other.y);
         }
 
-        Vector2 operator-(const Vector2 &other) const {
-            return Vector2(this->x - other.x, this->y - other.y);
+        constexpr Vector2 operator-(const Vector2 &other) const {
+            return Vector2(x - other.x, y - other.y);
         }
 
-        Vector2 operator*(const Vector2 &other) const {
-            return Vector2(this->x * other.x, this->y * other.y);
+        constexpr Vector2 operator*(const Vector2 &other) const {
+            return Vector2(x * other.x, y * other.y);
         }
 
-        Vector2 operator*(T other) const {
-            return Vector2(this->x * other, this->y * other);
+        constexpr Vector2 operator*(T other) const {
+            return Vector2(x * other, y * other);
         }
 
-        Vector2 operator/(T other) const {
-            return Vector2(this->x / other, this->y / other);
+        constexpr Vector2 operator/(T other) const {
+            return Vector2(x / other, y / other);
         }
 
-        Vector2 &operator+=(const Vector2 &other) {
-            this->x += other.x;
-            this->y += other.y;
+        constexpr Vector2 &operator+=(const Vector2 &other) {
+            x += other.x;
+            y += other.y;
             return *this;
         }
 
-        Vector2 &operator-=(const Vector2 &other) {
-            this->x -= other.x;
-            this->y -= other.y;
+        constexpr Vector2 &operator-=(const Vector2 &other) {
+            x -= other.x;
+            y -= other.y;
             return *this;
         }
 
-        Vector2 &operator*=(const Vector2 &other) {
-            this->x *= other.x;
-            this->y *= other.y;
+        constexpr Vector2 &operator*=(const Vector2 &other) {
+            x *= other.x;
+            y *= other.y;
             return *this;
         }
 
-        Vector2 &operator*=(const T other) {
-            this->x *= other;
-            this->y *= other;
+        constexpr Vector2 &operator*=(const T other) {
+            x *= other;
+            y *= other;
             return *this;
         }
 
-        Vector2 &operator/=(const T other) {
-            this->x /= other;
-            this->y /= other;
+        constexpr Vector2 &operator/=(const T other) {
+            x /= other;
+            y /= other;
             return *this;
         }
 
-        [[nodiscard]] T length() const {
-            return sqrt(this->x * this->x + this->y * this->y);
+        [[nodiscard]] constexpr T Length() const {
+            return std::sqrt(x * x + y * y);
         }
 
-        [[nodiscard]] T lengthSq() const {
-            return this->x * this->x + this->y * this->y;
+        [[nodiscard]] constexpr T LengthSq() const {
+            return x * x + y * y;
         }
 
-        [[nodiscard]] Vector2 normalized() const {
-            return Vector2(this->x / this->length(), this->y / this->length());
+        [[nodiscard]] constexpr Vector2 Normalized() const {
+            return Vector2(x / Length(),  y / Length());
         }
 
-        bool operator==(const Vector2 &other) const {
+        constexpr bool operator==(const Vector2 &other) const {
             return x == other.x && y == other.y;
         }
 
-        bool operator!=(const Vector2 &other) const {
+        constexpr bool operator!=(const Vector2 &other) const {
             return x != other.x || y != other.y;
         }
 
-        void clear() {
-            this->x = 0;
-            this->y = 0;
+        constexpr void Clear() {
+            x = 0;
+            y = 0;
+        }
+
+        constexpr T Dot(const Vector2 &other) const {
+            return x * other.x + y * other.y;
+        }
+
+        constexpr T Cross(const Vector2 &other) const {
+            return x * other.y - y * other.x;
         }
 
         T x;
